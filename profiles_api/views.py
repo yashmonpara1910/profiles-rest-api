72157fb1942e7998f932e5e658f9eb74de938993
Yash <yashmonpara11@gmail.com>
2020-09-25 22:00:31 +0530

    Add authentication

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Text API View"""
@@ -88,3 +92,14 @@ class HelloViewset(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """ Handle removing object """
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+   
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Profiles-rest-API
+
+profiles rest api code.
